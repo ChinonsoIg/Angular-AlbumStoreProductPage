b3f56da41c1a27ff6f735e80c68b5a41338576d9
ChinonsoIg <chinonso_ig@users.github.com>
2020-09-27 15:10:19 +0100

    creating a service to get album data - error fixed

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -15,7 +15,10 @@ export class ProductDescriptionComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response => {
+      
+      this.albumInfo = response
+    });
   }
 
 }
